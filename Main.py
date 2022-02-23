770cf48acc8430cd657e7a21ab5ab8c43ec3838d
JulioGoes <goes.cesar157@gmail.com>
2022-02-22 23:23:41 -0300

    update

--- Main.py
+++ Main.py
@@ -2,17 +2,22 @@ from Database.ConsultDatabase import consult_database
 import PySimpleGUI as sg
 
 teste = consult_database()
+cabecalho = ("Name", "Type", "Seller")
+sg.theme("Dark Red")
 
 layout = [
-    [sg.Text(teste[0])]
+    [sg.Table(teste, headings=cabecalho, justification="center")],
+    [sg.Button('Proximo')]
 ]
 
-window = sg.Window('RPG Store', layout, size=(400, 400))
+window = sg.Window('RPG Store', layout, size=(500, 300))
 
 while True:
 
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
+    if event == 'Proximo':
+        pass
 
 window.close()
