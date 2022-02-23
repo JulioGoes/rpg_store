8cc8a9aa1c8565e8e7685272550991146584edec
JulioGoes <goes.cesar157@gmail.com>
2022-02-22 21:43:10 -0300

    setup inicial

---
+++ Database/MakeDatabase.py
@@ -0,0 +1,37 @@
+from shutil import Error
+import sqlite3
+
+
+def create_connection(db_file):
+    connection = None
+    try:
+        connection = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return connection
+
+
+def create_table_products(connection):
+    cursor = connection.cursor()
+
+    cursor.execute('CREATE TABLE IF NOT EXISTS Products \
+        (name TEXT, type TEXT, seller TEXT)')
+
+    connection.commit()
+
+
+# create_table_products()
+def add_product_to_table(connection, produto):
+    cursor = connection.cursor()
+
+    sql = "INSERT INTO Products (name, type, seller) VALUES (?, ?, ?)"
+    cursor.execute(sql, produto)
+
+    connection.commit()
+    return cursor.lastrowid
+
+
+connection = create_connection('Database/database.db')
+# create_table_products(connection)
+add_product_to_table(connection, ('Dados Blend', 'Dado', 'Mercado RPG'))
